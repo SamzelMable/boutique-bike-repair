eee21fb2624e66c4875c92de96628809e92fa2be
Samuel Marle <samzelmable@gmail.com>
2025-11-05 13:50:00 +0100

    Fix router basename for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-<React.StrictMode>
-<BrowserRouter>
-<App />
-</BrowserRouter>
-</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
